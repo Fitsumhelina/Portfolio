5a7fcca660a3683774ce651112a9144b47640f72
Fitsumhelina <permin1501424@gmail.com>
2024-07-12 12:13:27 +0300

    directry an package update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outDir: 'build',
-
+  build: {
+    outDir: 'dist',
+  },
 })
